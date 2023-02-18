fc89886e9f52866628b1b8edfa7306155ee022c5
Het Shah <hetshah2828@gmail.com>
2023-02-18 18:32:34 +0530

    news fetching done

--- frontend/cricket-app/src/screen/homeScreen.js
+++ frontend/cricket-app/src/screen/homeScreen.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
-import "./HomeScreen.css";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../HomeScreen.css";
+import Header from "../components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 import MatchList from "../components/MatchList";
 import NewsList from "../components/NewsList";
 
 const HomeScreen = () => {
- 
-
   return (
     <>
-      <div className="contaier">
-        <MatchList/>
-        <NewsList/>
+      <Header />
+      <div className="d-md-flex">
+        <div class="col-md-8">
+          <MatchList />
+        </div>
+        <div class="col-md-4">
+          <NewsList />
+        </div>
       </div>
     </>
   );
